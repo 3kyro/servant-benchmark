fb31414fbb857e9af42aa0ca306a321718f3e865
3kyro <k.papachrisanthou@gmail.com>
2021-04-22 19:12:02 +0200

    Remove `Servant.Benchmark.Tools.Drill` from top imports

--- src/Servant/Benchmark.hs
+++ src/Servant/Benchmark.hs
@@ -9,15 +9,9 @@ module Servant.Benchmark (
     HasGenerator (..),
     HasEndpoint (..),
     Endpoint (..),
-
-    -- * Tools
-
-    -- | [Drill](https://github.com/fcsonline/drill) support
-    module Servant.Benchmark.Tools.Drill,
 ) where
 
 import Servant.Benchmark.Endpoint
 import Servant.Benchmark.Generator
 import Servant.Benchmark.HasEndpoint (HasEndpoint (..))
 import Servant.Benchmark.HasGenerator
-import Servant.Benchmark.Tools.Drill
